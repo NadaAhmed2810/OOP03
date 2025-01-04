27fd07d636c549e2e8197d229805b20071e81009
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-04 17:07:23 +0200

    Overloading

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace OOP03
+﻿namespace OOP03 
 {
     internal class Program
     {
+        #region Overloading
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static int Sum(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        static double Sum(int x, double y)
+        {
+            return x + y;
+        }
+
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region OOP Paller 3 Polymorphism 1.Overloading
+
+            //dynamic obj = "Nada";
+            //obj = 5;
+            //obj = 5.5;
+            //dynamic result =Sum(1,2);
+            //Console.WriteLine(result);
+            //result =       Sum(1, 2,3);
+            //Console.WriteLine(result);
+            //Console.WriteLine("Nada");//19 overload
+            //Console.WriteLine('A'); 
+            //Console.WriteLine(1);
+            //Console.WriteLineString(1);
+            //Console.WriteLineBool(1);
+            //Console.WriteLineChar(1);
+            //Console.WriteLineInt(1);
+
+            #endregion
+
+
         }
     }
 }
