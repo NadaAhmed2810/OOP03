bcf8c00f5ce0169430cbe32cbf04a0cf3e5790c6
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-04 17:45:30 +0200

    Binary Operator

---
+++ OOP03/Operators Overloading/Complex.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP03.Operators_Overloading
+{
+    internal class Complex
+    {
+        #region property
+        public int Real { get; set; }
+        public int Imag { get; set; }
+        #endregion
+        #region Binary Operator
+        //Overloading Operator:Must Be Not_Private And Class Member Function By logic:)
+        public static Complex operator +(Complex Left, Complex Right) {
+            return new Complex()
+            {
+                Real = (Left?.Real ?? 0) + (Right?.Real ?? 0),
+                Imag = (Left?.Imag ?? 0) + (Right?.Imag ?? 0)
+            };
+
+        }
+        public static Complex operator -(Complex Left, Complex Right)
+        {
+            return new Complex()
+            {
+                Real = (Left?.Real??0) - (Right?.Real ?? 0),
+                Imag = (Left?.Imag ?? 0) - (Right?.Imag ?? 0)
+            };
+
+        }
+       
+
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i";
+        }
+        #endregion
+
+    }
+}
--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace OOP03 
+﻿using OOP03.Operators_Overloading;
+
+
+namespace OOP03 
 {
     internal class Program
     {
@@ -42,7 +45,16 @@
 
             #endregion
 
-
+            Complex C1 = new Complex(){ Real=3,Imag=4};
+            Complex C2 = new Complex() { Real =6 , Imag = 8};
+            Complex C3=default;//null
+            C3 = C1 + C2;
+            C2 += C1;
+            Complex C4 = C1 + C2 + C3;
+            Console.WriteLine($"C1:{C1}");
+            Console.WriteLine($"C2:{C2}");
+            Console.WriteLine("---------");
+            Console.WriteLine($"C3:{C3}");
         }
     }
 }
