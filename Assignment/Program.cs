b837c400289135ddb5d94421a020fa1521035323
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-06 02:32:41 +0200

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -10,6 +10,16 @@
             //Console.WriteLine($"Sum of 3 Integer Number:{Calculator.Sum(1, 2,3)}");
             //Console.WriteLine($"Sum of 2 Double Number:{Calculator.Sum(1.5, 2.1)}");
             #endregion
+            #region Q2:Create a class named Rectangle with the following constructors
+            //Rectangle R1 = new Rectangle();
+            //Rectangle R2 = new Rectangle(1, 2);
+            //Rectangle R3= new Rectangle(3);
+            //Console.WriteLine(R1);
+            //Console.WriteLine();
+            //Console.WriteLine(R2);
+            //Console.WriteLine();
+            //Console.WriteLine(R3);
+            #endregion
             #endregion
         }
     }
---
+++ Assignment/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Rectangle
+    {
+   
+        public int Height { get;  }
+        public int Width{ get;  }
+        #region Constructors
+        public Rectangle()
+        {
+            Height = 0;
+            Width = 0;
+        }
+
+        public Rectangle(int Height)
+        {
+            this.Height = Height;
+            this.Width = Height;
+        }
+
+        public Rectangle(int Height, int Width)
+        {
+            this.Height = Height;
+            this.Width = Width;
+        }
+
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return $"Height:{Height}\nWidth:{Width}";
+        }
+        #endregion
+    }
+}
