0aad557f27f53122f2d1976b8d01d08de19d1a34
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-06 02:20:15 +0200

    Q1

---
+++ Assignment/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    abstract class Calculator
+    {
+        public static long Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public static long Sum(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+       
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01
+            #region Q1:Write a class named Calculator that contains a method named Add.
+            //Console.WriteLine($"Sum of 2 Integer Number:{Calculator.Sum(1, 2)}");
+            //Console.WriteLine($"Sum of 3 Integer Number:{Calculator.Sum(1, 2,3)}");
+            //Console.WriteLine($"Sum of 2 Double Number:{Calculator.Sum(1.5, 2.1)}");
+            #endregion
+            #endregion
+        }
+    }
+}
